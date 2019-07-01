0b39338ad6ff892ae997812f5e4a9e07b43107d0
Isac Haran de Almeida <isac.almeida@unoesc.edu.br>
2019-07-01 15:41:20 -0300

    Changes properties and sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT into pessoa (nome, data_nascimento, cpf, funcionario) VALUES
-('Bruno Fernandes Rodrigues', '28/01/1992', '000.111.222-333', true),
-('Fernando Gonzaga', '20/02/1997', '111.222.333-44', true),
-('Luiz Marenco', '27/05/1976', '222.333.444-55', false);
+('Bruno Fernandes Rodrigues', '1992-01-28', '000.111.222-333', true),
+('Fernando Gonzaga', '1997-02-20', '111.222.333-44', true),
+('Luiz Marenco', '1976-05-27', '222.333.444-55', false);
